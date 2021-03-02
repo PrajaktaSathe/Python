24c38ed9e8b5bee53c7e67bbe7d215b95106d3fa
Kenny <70860732+KennyOliver@users.noreply.github.com>
2021-03-02 14:49:42 +0000

    Update leap_year.py
    Hopefully you like this! 🤞

--- Projects/leap_year.py
+++ Projects/leap_year.py
@@ -1,19 +1,28 @@
-# take user input of year -
-year = int(input("Enter a year to check if leap year or not: "))
+def leap_check(year) -> str:
+  """ Check if a year is a leap year """
+  if ((year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0)):
+    return f"{year} is a leap year!"
+  else:
+    return f"{year} is not a leap year!"
+#====================
+# MAIN PROGRAM
+print("<-- Leap Year Checker -->")
+print("This program will check if a year you input is a leap year")
+print("\n")
 
-# initialize user_input to 1 -
-user_input = 1
-
-# continue until the user wants to stop - 
-while (user_input == 1):
-    # check condition of leap year -
-    if ((year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0)):
-        print("Entered year is a leap year!")
-        user_input = int(input("Do you want to continue?(1 for yes, 0 to stop): "))
-        if (user_input == 1):
-            year = int(input("Enter a year to check if leap year or not: "))
-    else:
-        print("Entered year is not a leap year!")
-        user_input = int(input("Do you want to continue?(1 for yes, 0 to stop): "))
-        if (user_input == 1):
-            year = int(input("Enter a year to check if leap year or not: "))
+run = 'Y'
+while run == 'Y': #runs once before user intervention
+  try:
+    input_year = int(input("Enter a year\n\t--> "))
+  except ValueError:
+    input_year = int(input("Must be an integer!\n\t--> "))
+  print(leap_check(input_year))
+  
+  print("-" * 30) #decoration
+  
+  print("Would you like to check another year?\n\t[Y] Yes\n\t[N] No")
+  run = input("\t--> ").upper()
+  while run not in ['Y','Yes','N','No']: #input validation
+    run = input("\t--> ").upper()
+  
+  print("-" * 30)l
