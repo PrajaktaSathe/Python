c2564fe1796bf04aac10fbf4bd1ac93c713bc936
Prajakta Sathe <63590390+PrajaktaSathe@users.noreply.github.com>
2021-03-17 18:53:45 +0530

    Add comments to code

--- Projects/calc.py
+++ Projects/calc.py
@@ -1,35 +1,44 @@
+# calculator function - which when called will function as a calculator
 def calculator():
     print("Options:\n\t[1] Add \n\t[2] Subtract \n\t[3] Multiply \n\t[4] Divide \n\t[5] Power \n\t[6] Square root")
-    ch = int(input("\t--> "))
+    ch = int(input("\t--> ")) # take option input from user
     
-    if ch == 1: #addition
+    #addition
+    if ch == 1: 
         num1 = float(input("1st number --> "))
         num2 = float(input("2nd number --> "))
         print(f"{num1} + {num2} = {num1 + num2}")
-    elif ch == 2: #subtraction
+    #subtraction
+    elif ch == 2: 
         num1 = float(input("1st number --> "))
         num2 = float(input("2nd number --> "))
         print(f"{num1} - {num2} = {num1 - num2}")
-    elif ch == 3: #multiplication
+    #multiplication
+    elif ch == 3: 
         num1 = float(input("1st number --> "))
         num2 = float(input("2nd number --> "))
         print(f"{num1} x {num2} = {num1 * num2}")
-    elif ch == 4: #division
+    #division
+    elif ch == 4: 
         num1 = float(input("Dividend --> "))
         num2 = float(input("Divisor  --> "))
+        # try-except which checks if divisor is zero (which isn't allowed)
         try:
           print(f"{num1} ÷ {num2} = {num1 / num2}")
         except ZeroDivisionError:
           print(f"{num1} ÷ {num2} = Error: Division by 0!")
-    elif ch == 5: #power
+    #power
+    elif ch == 5: 
         num = float(input("Number --> "))
         power = float(input("Power --> "))
         print(f"{num} ^ {power} = {num ** power}")
-    elif ch == 6: #root
+    #root
+    elif ch == 6: 
         num = float(input("Number --> "))
         print(f"√{num} = {num**(1/2)}")
     else:
         print("Invalid input!!")
+
 #====================
 # MAIN PROGRAM
 print("<-- Basic Calculator -->")
